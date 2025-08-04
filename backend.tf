14dfe45c86c94b32678b2f87ce2b93ff0ab1e003
Pratyushaa94 <prathyusha9449@gmail.com>
2025-08-04 17:52:58 +0530

    initial commit

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-pratyushaa-route-why"
-    key     = "route/dev/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket = "terraform-state-pratyushaa---why3"
+    key    = "ebs/dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
